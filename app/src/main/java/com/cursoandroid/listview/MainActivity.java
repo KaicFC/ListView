9874dfb046217fbf3ab65d570cd4db896be772cc
Kaic Carvalho <kaic15_fernandes@hotmail.com>
2021-01-06 14:20:24 -0300

    Criada a lista de locais para viajar. Feito o adaptador e criado o toast ao clickar na opçao desejada.

--- app/src/main/java/com/cursoandroid/listview/MainActivity.java
+++ app/src/main/java/com/cursoandroid/listview/MainActivity.java
@@ -3,12 +3,54 @@ package com.cursoandroid.listview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listLocais;
+    private String[] itens = {
+            "Angra dos Reis", "Caldas Novas", "Campos do Jordão", "Costa do Sauípe",
+            "Ilhéus", "Porto Seguro", "Tiradentes", "Praga", "Santiago", "Zurique",
+            "Caribe", "Buenos Aires", "Budapeste", "Cancún", "Aruba"
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listLocais = findViewById(R.id.listLocais);
+
+        //Criar adaptador para a Lista
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(
+                getApplicationContext(), //Contexto
+                android.R.layout.simple_list_item_1, //Escolha do Layout
+                android.R.id.text1, // ID dentro do simple_list_item
+                itens // Meu array
+        );
+
+        //Adicionar o adaptador na lista
+        listLocais.setAdapter(adaptador);
+
+        //Adicionar click na lista
+        listLocais.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                String valorSelecionado = listLocais.getItemAtPosition(position).toString();
+
+                Toast.makeText(getApplicationContext(), valorSelecionado, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
+
+
+
 }
